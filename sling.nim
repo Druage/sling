21cf0914062852cfb587e78dc743da225239ce25
Lee <Druage@gmx.com>
2016-03-04 15:15:26 -0500

    Supports void procs

--- sling.nim
+++ sling.nim
@@ -1,11 +1,11 @@
 import sequtils
 
 type
+
   slot* = void
   callback[T] = proc( t: T ): slot
   Signal*[T] = object
     connectionsList: seq[ callback[T] ]
-
 
 proc signal*[T](): Signal[T] =
   result.connectionsList = newSeq[ callback[T] ](0)
@@ -13,14 +13,13 @@ proc signal*[T](): Signal[T] =
 proc connect*[T, R]( self: var Signal[T], r: R ): void =
   self.connectionsList.add( r )
 
-#[
 proc disconnect*[T, S]( self: var Signal[T], cb: S ): void =
   for i in 0..len(self.connectionsList) - 1:
     var c = self.connectionsList[ i ]
     if c == cb:
       self.connectionsList.delete( i, i )
       break;
-]#
+
 proc clear*[T]( self: var Signal[T] ): void =
   self.connectionsList = newSeq[ callback[T] ](0)
 
@@ -28,25 +27,18 @@ proc emit*[T, R]( self: Signal[T], r: R ): void =
   for c in self.connectionsList:
     c( r )
 
+proc emit*[T]( self: Signal[T] ): void =
+  for c in self.connectionsList:
+    c()
+
 iterator connections*[T]( self: Signal[T] ): (callback[T]) =
   for c in self.connectionsList:
     yield c
 
 when isMainModule:
-  var sig = signal[int]()
+  var sig = signal[void]()
 
-  proc aslot( i: int ): slot =
-     echo("aslot recieved: ", i )
+  sig.connect( proc(): slot =
+                 echo("in proc") )
 
-  sig.connect( aslot )
-
-  sig.connect( proc( i: int ): slot =
-                echo( "lambda slot recieved: ", i ) )
-  sig.emit( 200 )
-
-  sig.clear()
-
-  sig.emit( 200 )
-
-
-  
+  sig.emit()
